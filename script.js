1ba3b25e2dfcc92983a9b8af7de43462bbdbb377
Austin <austinih46@gmail.com>
2023-02-07 11:20:17 -0600

    added Class Description as the first paragraph only

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const tester = document.querySelector('#tester')
 const stats = document.querySelectorAll('.stat')
 const abilScoreNames = ['STR','DEX','CON','INT','WIS','CHA']
 const ancestryBox = document.querySelector('#ancestryBox')
+const classBox = document.querySelector('#classBox')
+const classDescription = document.querySelector('#classDescription')
 
+//['Alchemist','Barbarian','Bard','Champion','Cleric','Druid','Fighter','Gunslinger','Inventor','Investigator','Magus','Monk','Oracle','Psychic','Ranger','Rogue','Sorcerer','Summoner','Swashbuckler','Thaumaturge','Witch','Wizard',']
+//[1,2,3,4,5,6,7,12,15,16,17,22]
 
 
 
@@ -36,27 +40,58 @@ const ancestryBox = document.querySelector('#ancestryBox')
         let ancestry = Math.floor(Math.random() * 36) + 1
         return ancestry
     }
-chooseAncestry()
+
+// class selection function
+    // const chooseClass = () => {
+    //     //classBox.textContent = event.target.textContent
+    //     classBox.textContent = `Class: ${event.target.textContent}`
+    //     return event.target.value
+        
+    // }
 
 async function getData(event) {
     event.preventDefault()
 
-    const url = 'https://api.pathfinder2.fr/v1/pf2/ancestry'
-    console.log(url)
+    let url1 = 'https://api.pathfinder2.fr/v1/pf2/ancestry'
 
-    fetch(url,
+    //Get Ancestry Data
+    fetch(url1,
         {method:'GET',
         headers:{'Authorization':'5cfe0fea-a504-4ee4-8f88-baf84aeabef2'}
-    })
+        })
         .then(res => {
             return res.json()})
         .then(res => {
+            
             let ancestryNum = chooseAncestry()
             console.log(ancestryNum)
             ancestryBox.textContent = `Ancestry: ${res.results[ancestryNum].name}`
         })
         .catch(err => {
             console.log("error!", err)})
+    
+    // Get Class Data
+    let url2 = 'https://api.pathfinder2.fr/v1/pf2/class'       
+    fetch(url2,
+        {method:'GET',
+        headers:{'Authorization':'5cfe0fea-a504-4ee4-8f88-baf84aeabef2'}
+        })
+        .then (res => {
+            return res.json()})
+        .then(res => {
+            // let classNum = chooseClass()
+            // classBox.textContent = `Class: ${event.target.textContent}`
+            let classNum = event.target.value
+            classBox.textContent = `Class: ${res.results[classNum].name}`
+            let classDesc = res.results[classNum].system.description.value
+            classDesc = classDesc.split("</em")
+            classDescription.textContent = classDesc[0].replace('<p><em>','')
+                ///the following site taught me how to split text so that only the first paragraph would be extracted: https://stackoverflow.com/questions/61791863/how-to-extract-the-content-of-the-first-paragraph-in-html-string-react-native
+        })
+        
+        .catch(err => {
+            console.log("error!", err)})          
+
 }
 
 
@@ -66,4 +101,4 @@ const test = function() {
 
 alchemistBtn.addEventListener('click',populateStats)
 alchemistBtn.addEventListener('click',getData)
-alchemistBtn.addEventListener('click',)
+// alchemistBtn.addEventListener('click',chooseClass)
